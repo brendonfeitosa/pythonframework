8f3a15242c2f8125343f880a6efc033c84952f8e
Brendon Gustavo Feitosa <Brendon Gustavo Feitosa>
2025-09-02 20:17:12 -0300

    teste tkinter 2

--- Aula 02.09.2025/app.py
+++ Aula 02.09.2025/app.py
@@ -1,7 +1,11 @@
 from tkinter import *
 from tkinter import ttk
-root = Tk()
-frm = ttk.Frame(root, padding=10)
+root = Tk() #aqui cria um objeto através da classe Tk
+frm = ttk.Frame(root, padding=10) #criou um objeto do tipo frame, e estou falando qu quero configurar ele dentro do root
+
+
+
+
 frm.grid()
 ttk.Label(frm, text="Hello World!").grid(column=0, row=0)
 ttk.Button(frm, text="Quit", command=root.destroy).grid(column=1, row=0)
@@ -9,4 +13,6 @@ ttk.Label(frm, text="Hello World!").grid(column=0, row=1)
 ttk.Button(frm, text="Quit", command=root.destroy).grid(column=1, row=1)
 ttk.Label(frm, text="Hello World!").grid(column=0, row=2)
 ttk.Button(frm, text="Quit", command=root.destroy).grid(column=1, row=2)
+
+
 root.mainloop()
