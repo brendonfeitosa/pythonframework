ed333970847e10bf7fc2b920918b54fe2c236b01
Brendon Gustavo Feitosa <Brendon Gustavo Feitosa>
2025-09-23 20:34:11 -0300

    Começando a trabalhar com POO em Pyhton 2

---
+++ POO/app.py
@@ -0,0 +1,6 @@
+from core.carro.carro import Carro
+
+#instanciar a classe para construir o objeto
+obj_honda_civic_xls = Carro(5, 5, 2, 140)
+
+obj_honda_civic_exs = Carro(5, 5, 2, 180)
--- POO/core/carro/carro.py
+++ POO/core/carro/carro.py
@@ -1,2 +1,12 @@
 class Carro:
+    def __init__(self, rodas, pneu, retrovisor, motor): #construtor de uma classe em python self diz para construir a propria classe, estou referenciando ela os argumentos são obrigatórios
+        self.rodas = rodas
+        self.pneu = pneu
+        self.retrovisor = retrovisor
+        self.motor = motor
+
+    def ligar_motor(self):
+        return "motor ligado..."
     
+    def acelerar_carro(self):
+        return "carro acelerado..."
