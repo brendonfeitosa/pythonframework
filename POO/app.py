e527f08a0aeea90f7d1689e95d0bfc4ea16abb0e
Brendon Gustavo Feitosa <Brendon Gustavo Feitosa>
2025-09-23 21:29:30 -0300

    Finalizado exemplo de POO

--- POO/app.py
+++ POO/app.py
@@ -1,6 +1,25 @@
 from core.carro.carro import Carro
 
 #instanciar a classe para construir o objeto
-obj_honda_civic_xls = Carro(5, 5, 2, 140)
+obj_honda_civic_xls = Carro(motor=140, modelo="xls")
 
-obj_honda_civic_exs = Carro(5, 5, 2, 180)
+obj_honda_civic_exs = Carro(motor=180, modelo="exs")
+
+obj_honda_civic_lx = Carro(motor=100, modelo="lx")
+
+#utilizar
+
+cavalos = obj_honda_civic_exs.motor
+modelo = obj_honda_civic_exs.modelo
+rodas = obj_honda_civic_exs.rodas
+print(f"Qtd Cavalos do modelo {modelo} : {cavalos} rodas {rodas}")
+
+result = obj_honda_civic_exs.ligar_motor(False)
+print(result)
+
+result_2 = obj_honda_civic_exs.acelerar_carro()
+print(result_2)
+
+motor, modelo = obj_honda_civic_exs.get_informacoes()
+
+print(f"INFO: motor ({motor}) modelo ({modelo})")
--- POO/core/carro/carro.py
+++ POO/core/carro/carro.py
@@ -1,12 +1,23 @@
+#classe Carro
 class Carro:
-    def __init__(self, rodas, pneu, retrovisor, motor): #construtor de uma classe em python self diz para construir a propria classe, estou referenciando ela os argumentos são obrigatórios
+    #Construtor da classe
+    def __init__(self, rodas=5, pneu=5, retrovisor=2, motor=0, modelo=""): #construtor de uma classe em python self diz para construir a propria classe, estou referenciando ela os argumentos são obrigatórios
+        #propriedades/atributos da classe
         self.rodas = rodas
         self.pneu = pneu
         self.retrovisor = retrovisor
         self.motor = motor
+        self.modelo = modelo
 
-    def ligar_motor(self):
-        return "motor ligado..."
+    #métodos da classe
+    def ligar_motor(self, empurrar):
+        if empurrar:
+            return "ligou a chave, empurrou e o motor pegou no tranco..."
+        else:
+            return "motor ligado pela chave..."
     
     def acelerar_carro(self):
         return "carro acelerado..."
+    
+    def get_informacoes(self):
+        return self.motor, self.modelo
